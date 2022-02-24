1426742b1cdb072f48dd557188557f5d6070c16e
DESKTOP-H2SPHM1\utente <cluca98@live.it>
2022-02-24 12:41:49 +0100

    quasi nulla

--- egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/ui/main/ProfileFragment.kt
+++ egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/ui/main/ProfileFragment.kt
@@ -1,29 +1,58 @@
 package com.example.egreen_fragmentapplication.ui.main
 
+import android.app.Activity.RESULT_OK
+import android.app.appsearch.AppSearchResult.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Switch
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.media.MediaBrowserServiceCompat.RESULT_OK
 import androidx.navigation.fragment.findNavController
+import com.example.egreen_fragmentapplication.MainActivity
 import com.example.egreen_fragmentapplication.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
+    }}
 
+       /*
 
+        TENTATIVO FALLITO
 
+        val PICK_IMAGE_REQUEST = 1
 
+        val changePicBtn = view.findViewById<Button>(R.id.change_pic_btn)
+        val uploadPicBtn = view.findViewById<Button>(R.id.upload_pic_btn)
+        val profilePic = view.findViewById<ImageView>(R.id.profile_pic)
+        var imageUri : String = ""
+
+        changePicBtn.setOnClickListener{
+            openFileChooser()
+        }
 
     }
-}
+
+    fun openFileChooser(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        getResult.launch(intent)
+
+    }
+    val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == MainActivity.RESULT_OK){
+                val value = it.data?.getStringExtra("input")
+            }
+        }
+
+        */*/
