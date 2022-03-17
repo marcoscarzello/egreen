71ae858f93a1d33c26037d5a87638f5969c5abff
DESKTOP-H2SPHM1\utente <cluca98@live.it>
2022-03-17 16:38:25 +0100

    prova notifica

--- egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/MainActivity.kt
+++ egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.egreen_fragmentapplication
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
@@ -19,9 +26,47 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+
+    //prova notifica
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationID = 101
+
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply{
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Pianta innaffiata!")
+            .setContentText("La tua pianta è stata appena innaffiata dal sistema per carenza di umidità.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationID, builder.build())
+        }
+    }
+    //
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        //notifica prova
+        createNotificationChannel()
+        val notificaBtn= findViewById<TextView>(R.id.notifica)
+        notificaBtn.setOnClickListener{
+            sendNotification()
+        }
+        //
 
 
         hideBottomBar(false)
@@ -49,6 +94,9 @@ class MainActivity : AppCompatActivity() {
 
 
         val nav = findViewById<BottomNavigationView>(R.id.bottomNavigationView1)
+
+
+
 
         nav.setOnItemSelectedListener{ item ->
             when(item.itemId){
