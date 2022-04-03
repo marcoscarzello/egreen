07c417e300cf8e67013e98e578cc085d6879fb32
Giorgio <giorgios94@hotmail.it>
2022-04-03 03:40:57 +0200

    altre modifiche

--- egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/MainActivity.kt
+++ egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/MainActivity.kt
@@ -60,6 +60,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        supportActionBar?.hide()
 
         //notifica prova
         createNotificationChannel()
