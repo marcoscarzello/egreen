3941e23a5c77d799190e2dd33f3f46c5d0051c4a
LAPTOP-3POE6MU6\laradice <lara.fantone@studenti.polito.it>
2022-06-26 18:04:44 +0200

    clear stack dell'activity dopo logout

--- egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/ui/main/AccountSettingsFragment.kt
+++ egreen_fragmentApplication/app/src/main/java/com/example/egreen_fragmentapplication/ui/main/AccountSettingsFragment.kt
@@ -1,21 +1,15 @@
 package com.example.egreen_fragmentapplication.ui.main
 
-import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
-import android.content.ContentValues.TAG
 import android.content.DialogInterface
 import android.content.Intent
-import android.media.Image
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -107,6 +101,11 @@ class AccountSettingsFragment : Fragment(R.layout.fragment_account_settings) {
                 "You're logged out successfully",
                 Toast.LENGTH_SHORT
             ).show()
+
+            //clear stack
+            val intent = Intent(view.context, this@AccountSettingsFragment.requireActivity()::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
 
         //TO GARDEN SETTINGS
@@ -129,12 +128,19 @@ class AccountSettingsFragment : Fragment(R.layout.fragment_account_settings) {
                 .setPositiveButton("I'm sure", DialogInterface.OnClickListener {
                         dialog, id ->
                     viewModel.deleteAccount()
+
                     findNavController().navigate(R.id.action_accountSettingsFragment_to_registerFragment)
                     Toast.makeText(
                         this@AccountSettingsFragment.requireContext(),
                         "Account deleted",
                         Toast.LENGTH_SHORT
                     ).show()
+
+                    //clear stack
+                    val intent = Intent(view.context, this@AccountSettingsFragment.requireActivity()::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+
                 })
                 // negative button text and action
                 .setNegativeButton("My mistake", DialogInterface.OnClickListener {
